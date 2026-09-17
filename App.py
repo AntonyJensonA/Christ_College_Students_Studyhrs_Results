6b9f0328e5ae8fb720f346effa428d74693d7d27
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-17 11:22:51 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,14 +1,18 @@
 import streamlit as st
+import pandas as pd
 import joblib
 
 model = joblib.load("logistic_regression_student_study_model.pkl")
 
 st.title("Student Pass / Fail Based on Study Hours")
 hours = st.number_input("Enter the study hours" , min_value =0.0 ,max_value = 15.0 ,value=5.0)
+attendance = st.number_input("Enter the attendance" , min_value =0.0 ,max_value = 100.0,value=75.0)
+
 
 if st.button("Predict"):
-  prediction = model.predict([[hours]])
-  probability = model.predict_proba([[hours]])
+  # input_data = pd.DataFrame(
+  prediction = model.predict([["StudyHours","Attendance"]])
+  probability = model.predict_proba([["StudyHours","Attendance"]])
   if prediction[0] == 1:
     st.success("Pass")
   else:
