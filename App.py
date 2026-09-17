571c29c259618385837202911cac4e4189b59eda
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-17 12:36:06 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ model = joblib.load("Price_Model.pkl")
 
 st.title("Student Pass / Fail Based on Study Hours")
 area = st.number_input("Enter the area in sq_ft" , min_value = 600.00 ,max_value = 3000.0 ,value=610.0)
-if area <600.00 or area>300.00:
+if area <600.00 or area>3000.00:
   st.error("Area should be between 600 and 3000 sq ft")
 bedroom = st.number_input("Enter the number of bedrooms" , min_value =1.0 ,max_value = 4.0,value=2.0)
 if bedroom <1.0 or bedroom>4.0:
