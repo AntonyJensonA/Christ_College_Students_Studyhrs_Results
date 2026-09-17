daab9b22a8de7acab60076b70a2e8d7064fcd014
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-17 10:38:44 +0530

    Fix typo in probability variable name

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ hours = st.number_input("Enter the study hours" , min_value =0.0 ,max_value = 15
 
 if st.button("Predict"):
   prediction = model.predict([[hours]])
-  probablity = model.predict_proba([[hours]])
+  probability = model.predict_proba([[hours]])
   if prediction[0] == 1:
     st.success("Pass")
   else:
