b2b93e6bf43d5fa267ced672d3537c5b08799c61
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-17 09:29:09 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+
+model = joblib.load("logistic_regression_student_study_model.pkl")
+
+st.title("Student Pass / Fail Based on Study Hours")
+hours = st.number_input("Enter the study hours" , min_value =0.0 ,max_value = 15.0 ,value=5.0)
+
+if st.button("Predict"):
+  prediction = model.predict([[hours]])
+  if prediction[0] == 1:
+    st.success("Pass")
+  else:
+    st.error("Fail")
