0ad0ebf7e64f1f5cf7b6535b8af29f609004295e
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-17 11:27:33 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,11 +8,14 @@ st.title("Student Pass / Fail Based on Study Hours")
 hours = st.number_input("Enter the study hours" , min_value =0.0 ,max_value = 15.0 ,value=5.0)
 attendance = st.number_input("Enter the attendance" , min_value =0.0 ,max_value = 100.0,value=75.0)
 
+input_data = pd.DataFrame({
+  "StudyHours":[hours],
+  "Attendance":[attendance]
+})
 
 if st.button("Predict"):
-  # input_data = pd.DataFrame(
-  prediction = model.predict([["StudyHours","Attendance"]])
-  probability = model.predict_proba([["StudyHours","Attendance"]])
+  prediction = model.predict(input_data)
+  probability = model.predict_proba(input_data)
   if prediction[0] == 1:
     st.success("Pass")
   else:
